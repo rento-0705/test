23ee3e1057156746360236013025344804f85ad4
rento-0705 <60836960+rento-0705@users.noreply.github.com>
2020-06-16 07:30:25 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+alert("warning! Your terminal has accessed https://rento-0705.github.io/test.")
+
+alert("Continue warning")
+
+alert("systemScan")
+
+alert("OK")
+
+alert("Warning! A virus has been detected on your device. Name=Tap Snake")
