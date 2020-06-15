c2b6f289f287fc5e08b3f1502f8a753dc4c30034
rento-0705 <60836960+rento-0705@users.noreply.github.com>
2020-06-16 07:38:01 +0900

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-alert("warning! Your terminal has accessed https://rento-0705.github.io/test.")
-
-alert("Continue warning")
-
-alert("systemScan")
-
-alert("OK")
-
-alert("Warning! A virus has been detected on your device. Name=Tap Snake")
